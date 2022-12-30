8b429b36a4c9e5b2ac84153d4aa75a9f8eec5361
Tejas Hiremath <40001472+tejas642@users.noreply.github.com>
2022-12-31 00:26:06 +0530

    Added DrumKit Code

---
+++ index.js
@@ -0,0 +1,64 @@
+var numberofsounds = document.querySelectorAll("button").length;
+for (var i = 0; i < numberofsounds; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var key = this.innerHTML;
+    makesound(key);
+    animation(key);
+  });
+}
+
+function makesound(key) {
+  key = key.toLowerCase();
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
+
+document.addEventListener("keydown", keypressed);
+function keypressed(x) {
+  makesound(x.key);
+  animation(x.key);
+}
+
+function animation(x) {
+  document.querySelector("." + x).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector("." + x).classList.remove("pressed");
+  }, 100);
+}
